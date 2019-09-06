38ab66759d972dd526e079c8de1b75d44a37ffe6
scottmacaz <scottmacaz@yahoo.com>
2019-09-05 17:31:44 -0700

    Map version of space age problem

--- go/space-age/space_age.go
+++ go/space-age/space_age.go
@@ -8,44 +8,25 @@ type Planet string
 // Age takes an age in seconds and a planet.  It returns an age in years
 // on the given planet.
 func Age(ageInSeconds float64, planet Planet) float64 {
-	//The following variables are percentage of earth year.
-	mercuryOrbitalPercentage := 0.2408467
-	venusOrbitalPercentage := 0.61519726
-	marsOrbitalPercentage := 1.8808158
-	jupiterOrbitalPercentage := 11.862615
-	saturnOrbitalPercentage := 29.447498
-	uranusOrbitalPercentage := 84.016846
-	neptuneOrbitalPercentage := 164.79132
+	//The following floats are percentage of earth year.
+	orbitalMap := make(map[Planet]float64)
+	orbitalMap["Earth"] = 1.0
+	orbitalMap["Mercury"] = 0.2408467
+	orbitalMap["Venus"] = 0.61519726
+	orbitalMap["Mars"] = 1.8808158
+	orbitalMap["Jupiter"] = 11.862615
+	orbitalMap["Saturn"] = 29.447498
+	orbitalMap["Uranus"] = 84.016846
+	orbitalMap["Neptune"] = 164.79132
 
-	switch planet {
-	case "Earth":
-		return determineRelativeAge(ageInSeconds, 1)
-	case "Mercury":
-		return determineRelativeAge(ageInSeconds, mercuryOrbitalPercentage)
-	case "Venus":
-		return determineRelativeAge(ageInSeconds, venusOrbitalPercentage)
-	case "Mars":
-		return determineRelativeAge(ageInSeconds, marsOrbitalPercentage)
-	case "Jupiter":
-		return determineRelativeAge(ageInSeconds, jupiterOrbitalPercentage)
-	case "Saturn":
-		return determineRelativeAge(ageInSeconds, saturnOrbitalPercentage)
-	case "Uranus":
-		return determineRelativeAge(ageInSeconds, uranusOrbitalPercentage)
-	case "Neptune":
-		return determineRelativeAge(ageInSeconds, neptuneOrbitalPercentage)
-	default:
-		panic(fmt.Sprintf("Unrecognized planet: %v", planet))
+	if val, ok := orbitalMap[planet]; ok {
+		return determineRelativeAge(ageInSeconds, val)
 	}
+	panic(fmt.Sprintf("Unrecognized planet: %v", planet))
 }
 
 func determineRelativeAge(ageInSeconds float64, planetOrbitalPercentage float64) float64 {
-	return ageInSeconds / calculateSecondsInYear(planetOrbitalPercentage)
-}
-
-func calculateSecondsInYear(earthOrbitalPercent float64) float64 {
 	secondsInDay := float64(86400)
 	earthDaysInYear := 365.25
-
-	return earthOrbitalPercent * earthDaysInYear * secondsInDay
+	return ageInSeconds / (planetOrbitalPercentage * earthDaysInYear * secondsInDay)
 }
