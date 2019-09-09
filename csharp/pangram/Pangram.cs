143cec902f57b2f1fe3ff9bf6a861efeed6bfbcc
Scott MacMillan <scott.macmillan@hamiltoncompany.com>
2019-09-09 13:37:08 -0700

    CSharp Pangram exercise

---
+++ csharp/pangram/Pangram.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+      var charsFound = new HashSet<char>();
+        foreach (var c in input)
+        {
+          if (Char.IsLetter(c))
+          {
+            charsFound.Add(char.ToLower(c));
+          }
+        }
+      return charsFound.Count == 26;
+    }
+}
