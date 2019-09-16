464862d0511e45a6cca252c080f0a71db3f9dc7f
Scott MacMillan <scott.macmillan@hamiltoncompany.com>
2019-09-16 10:17:47 -0700

    CSharp Twofer exercise

---
+++ csharp/two-fer/TwoFer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public static class TwoFer
+{
+    public static string Speak(string name=null)
+    {
+      return name == null ? respond("you") : respond(name);
+    }
+    private static string respond(string name)
+    {
+      return $"One for {name}, one for me.";
+    }
+}
