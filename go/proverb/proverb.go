c861640e5be23fca5247168c451ac6a7cc319f79
Scott MacMillan <scott.macmillan@hamiltoncompany.com>
2019-09-17 14:25:10 -0700

    Updated Go Proverb solution

--- go/proverb/proverb.go
+++ go/proverb/proverb.go
@@ -13,11 +13,9 @@ func Proverb(rhyme []string) []string {
 
 	for i := 0; i < len(rhyme); i++ {
 		if i != len(rhyme)-1 {
-			line := fmt.Sprintf("For want of a %s the %s was lost.", rhyme[i], rhyme[i+1])
-			proverb = append(proverb, line)
+			proverb = append(proverb, fmt.Sprintf("For want of a %s the %s was lost.", rhyme[i], rhyme[i+1]))
 		} else {
-			lastLine := fmt.Sprintf("And all for the want of a %s.", rhyme[0])
-			proverb = append(proverb, lastLine)
+			proverb = append(proverb, fmt.Sprintf("And all for the want of a %s.", rhyme[0]))
 		}
 	}
 	return proverb
