0c78eecea0dcc5822065c0d2b639b21816addd59
Scott MacMillan <scott.macmillan@hamiltoncompany.com>
2019-09-17 15:03:52 -0700

    C# isogram minor update

--- csharp/isogram/Isogram.cs
+++ csharp/isogram/Isogram.cs
@@ -1,24 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public static class Isogram
 {
-    public static bool IsIsogram(string word)
+  public static bool IsIsogram(string word)
+  {
+    var charsFound = new List<char>();
+    foreach (char c in word)
     {
-      var charsFound = new List<char>();
-      foreach(char c in word) 
+      if (char.IsLetter(c))
       {
-        //Only test for letters.
-        if (!char.IsLetter(c))
-        {
-          continue;
-        }
         if (charsFound.Contains(char.ToLower(c)))
         {
           return false;
         }
         charsFound.Add(char.ToLower(c));
       }
-      return true;
     }
+    return true;
+  }
 }
