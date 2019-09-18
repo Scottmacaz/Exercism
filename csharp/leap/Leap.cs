0843e1834b1fef94d5967203f5fd349be0c599e5
Scott MacMillan <scott.macmillan@hamiltoncompany.com>
2019-09-18 06:40:32 -0700

    Updated to one line

--- csharp/leap/Leap.cs
+++ csharp/leap/Leap.cs
@@ -2,12 +2,6 @@ using System;
 
 public static class Leap
 {
-    public static bool IsLeapYear(int year)
-    {
-        var yearDivisibleBy4 = year % 4 == 0;
-        var yearDivisibleBy100 = year % 100 == 0;
-        var yearDivisibleBy400 = year % 400 == 0;
+  public static bool IsLeapYear(int year) => (year % 4 <= 0) && (year % 100 != 0 || year % 400 == 0);
 
-        return yearDivisibleBy4 && !yearDivisibleBy100 || yearDivisibleBy400;
-    }
 }
