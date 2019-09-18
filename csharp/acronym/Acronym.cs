ea65d9da4bb75b7d16b9e147ce66548b1f3712d0
Scott MacMillan <scott.macmillan@hamiltoncompany.com>
2019-09-18 08:09:16 -0700

    Final C# acronym exercise

--- csharp/acronym/Acronym.cs
+++ csharp/acronym/Acronym.cs
@@ -2,41 +2,32 @@
 
 public static class Acronym
 {
-    public static string Abbreviate(string phrase)
+  public static string Abbreviate(string phrase)
+  {
+    var words = phrase.Split(" ");
+    var acronym = string.Empty;
+    foreach (var word in words)
     {
-        var words = phrase.Split(" ");
-        var acronym = string.Empty;
-        foreach (var word in words)
-        {
-          acronym += GetAcronymLetters(word);
-        }
-        return acronym;
+      acronym += GetAcronymLetters(word);
+    }
+    return acronym;
+  }
+
+  private static string GetAcronymLetters(string word)
+  {
+    string acronymLetters = string.Empty;
+    //If it's a single character and not a letter then it's not part of the acronym.
+    if (word.Length == 1 && char.IsLetter(word[0]) == false)
+    {
+      return string.Empty;
     }
 
-    private static string GetAcronymLetters(string word)
+    var hyphenWords = word.Split("-");
+    foreach (var hyphenWord in hyphenWords)
     {
-      string acronymLetters = string.Empty;
-      //If it's a single character and not a letter then it's not part of the acronym.
-      if (word.Length == 1 && char.IsLetter(word[0]) == false)
-      {
-        return string.Empty;
-      }
-      
-      var hyphenWords = word.Split("-");
-      foreach(var hyphenWord in hyphenWords)
-      {
-        if( char.IsLetter(hyphenWord[0]))
-        {
-          acronymLetters +=  char.ToUpper(hyphenWord[0]);
-        }
-        else 
-        {
-          acronymLetters += char.ToUpper(hyphenWord[1]);
-        }
-      }
-      return acronymLetters;
-          
-      
+      acronymLetters += char.IsLetter(hyphenWord[0]) ? char.ToUpper(hyphenWord[0]) : char.ToUpper(hyphenWord[1]);
     }
-    
+    return acronymLetters;
+  }
+
 }
