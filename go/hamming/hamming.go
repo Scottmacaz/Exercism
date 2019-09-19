504302e14da248243d15195ba5c9b50da521fbed
Scott MacMillan <scott.macmillan@hamiltoncompany.com>
2019-09-19 08:34:11 -0700

    Go Hamming exercise solution

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,4 +1,20 @@
 package hamming
 
+import (
+	"errors"
+)
+
+// Distance is a function counting the number of differences between two homologous DNA strands
 func Distance(a, b string) (int, error) {
+
+	if len(a) != len(b) {
+		return -1, errors.New("string lengths must be the same")
+	}
+	hd := 0 //hamming distance
+	for i, ai := range a {
+		if uint8(ai) != b[i] {
+			hd++
+		}
+	}
+	return hd, nil
 }
