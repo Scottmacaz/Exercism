221eb8f04aafbc9cf0ab2aefdeca4fa5ee32309d
Scott MacMillan <scott.macmillan@hamiltoncompany.com>
2019-09-19 06:02:32 -0700

    Updated C# acronym exercise to use linq

--- csharp/acronym/Acronym.cs
+++ csharp/acronym/Acronym.cs
@@ -1,33 +1,11 @@
 ﻿using System;
+using System.Linq;
 
 public static class Acronym
 {
   public static string Abbreviate(string phrase)
   {
-    var words = phrase.Split(" ");
-    var acronym = string.Empty;
-    foreach (var word in words)
-    {
-      acronym += GetAcronymLetters(word);
-    }
-    return acronym;
+    var words = phrase.Split(new char[] { ' ', '_', '-' }, StringSplitOptions.RemoveEmptyEntries);
+    return string.Join("", words.Select(c => char.ToUpper(c[0])));
   }
-
-  private static string GetAcronymLetters(string word)
-  {
-    string acronymLetters = string.Empty;
-    //If it's a single character and not a letter then it's not part of the acronym.
-    if (word.Length == 1 && char.IsLetter(word[0]) == false)
-    {
-      return string.Empty;
-    }
-
-    var hyphenWords = word.Split("-");
-    foreach (var hyphenWord in hyphenWords)
-    {
-      acronymLetters += char.IsLetter(hyphenWord[0]) ? char.ToUpper(hyphenWord[0]) : char.ToUpper(hyphenWord[1]);
-    }
-    return acronymLetters;
-  }
-
 }
