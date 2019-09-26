08cea9de2031123fd19c0e00a36e148df2278d92
Scott MacMillan <scott.macmillan@hamiltoncompany.com>
2019-09-26 06:56:41 -0700

    Update to hamming.go

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -7,12 +7,14 @@ import (
 // Distance is a function counting the number of differences between two homologous DNA strands
 func Distance(a, b string) (int, error) {
 
-	if len(a) != len(b) {
-		return -1, errors.New("string lengths must be the same")
-	}
 	hd := 0 //hamming distance
-	for i, ai := range a {
-		if uint8(ai) != b[i] {
+	ar, br := []rune(a), []rune(b)
+	if len(ar) != len(br) {
+		return 0, errors.New("string lengths must be the same")
+	}
+
+	for i, ai := range ar {
+		if ai != br[i] {
 			hd++
 		}
 	}
