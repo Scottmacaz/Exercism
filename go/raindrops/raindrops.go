8c4da050a09e56a81790e0a1cfceb200492e51b4
Scott MacMillan <scott.macmillan@hamiltoncompany.com>
2019-09-26 06:57:01 -0700

    Go raindrops initial solution

---
+++ go/raindrops/raindrops.go
@@ -0,0 +1,25 @@
+package raindrops
+
+import "strconv"
+
+//Convert takes a raindrop int and returns the corresponding string
+func Convert(r int) string {
+	rs := "" //The raindrop string
+
+	if r%3 == 0 {
+		rs += "Pling"
+	}
+
+	if r%5 == 0 {
+		rs += "Plang"
+	}
+	
+	if r%7 == 0 {
+		rs += "Plong"
+	}
+
+	if len(rs) == 0 {
+		rs = strconv.Itoa(r)
+	}
+	return rs
+}
