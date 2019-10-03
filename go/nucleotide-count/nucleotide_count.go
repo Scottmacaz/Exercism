ae3eb319bdeaaeb6bacda972f09fff67b8b029f5
Scott MacMillan <scott.macmillan@hamiltoncompany.com>
2019-10-03 06:46:36 -0700

    Go Necleotide removed switch and added check for existence in dictionary

--- go/nucleotide-count/nucleotide_count.go
+++ go/nucleotide-count/nucleotide_count.go
@@ -5,13 +5,13 @@ import "errors"
 // Histogram is a mapping from nucleotide to its count in given DNA.
 type Histogram map[rune]int
 
-// DNA is a list of nucleotides. 
+// DNA is a list of nucleotides.
 type DNA string
 
 // Counts generates a histogram of valid nucleotides in the given DNA.
 // Returns an error if d contains an invalid nucleotide.
 func (d DNA) Counts() (Histogram, error) {
-	h := map[rune]int{
+	var h = Histogram{
 		'A': 0,
 		'C': 0,
 		'G': 0,
@@ -19,12 +19,10 @@ func (d DNA) Counts() (Histogram, error) {
 	}
 
 	for _, c := range d {
-		switch c {
-		case 'A', 'C', 'G', 'T':
-			h[c]++
-		default:
+		if _, ok := h[c]; !ok {
 			return nil, errors.New("Invalid nucleotide")
 		}
+		h[c]++
 	}
 
 	return h, nil
