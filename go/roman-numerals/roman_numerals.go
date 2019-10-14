25c4b3a34ce3c13edabecce101ce5d2a12369889
Scott MacMillan <scott.macmillan@hamiltoncompany.com>
2019-10-14 05:53:58 -0700

    Start of Roman Numeral exercise. Code compiles

---
+++ go/roman-numerals/roman_numerals.go
@@ -0,0 +1,22 @@
+package romannumerals
+
+func ToRomanNumeral(a int) (string, error) {
+
+	return "blah", nil
+}
+
+
+//This is the code that converts an int to a group of places:
+// func main() {
+// 	x := 43928
+// 	i := 10
+
+// 	for x > i/10 {
+// 		place := x%i - x%(i/10)
+// 		fmt.Println("Place: ", place)
+// 		i *= 10
+// 	}
+
+// }
+// func getPlaces(arabic int) []int {
+// }
