0bbd5b09f9086cea98b57764c90050489533d10d
Scott MacMillan <scott.macmillan@hamiltoncompany.com>
2019-10-21 14:33:54 -0700

    Go roman numeral exercise working

--- go/roman-numerals/roman_numerals.go
+++ go/roman-numerals/roman_numerals.go
@@ -1,73 +1,84 @@
 package romannumerals
 
-// ToRomanNumeral is a function blah ...
+import (
+	"strconv"
+	"strings"
+	"errors"
+)
+
+// ToRomanNumeral is a function that will convert a 
+// number between 1 and 3000 to a roman numeral.
 func ToRomanNumeral(a int) (string, error) {
-	//places := getPlaces(a)
-	return "blah", nil
+
+	if (a <= 0 || a > 3000) {
+		return "",  errors.New("Number to convert must be between 1 and 3000")
+	}
+	places := getPlaces(a)
+	rn := ""
+	for _, p := range places {
+		rnu := makeRomanNumeral(p)
+		rn += rnu
+	}
+	return rn, nil
+}
+func makeRomanNumeral(arabic int) string {
+	rn := ""
+	for arabic > 0 {
+		switch {
+		case arabic >= 1000:
+			arabic -= 1000
+			rn += "M"
+		case arabic >= 900:
+			arabic -= 900
+			rn += "CM"
+		case arabic >= 500:
+			arabic -= 500
+			rn += "D"
+		case arabic >= 400:
+			arabic -= 400
+			rn += "CD"
+		case arabic >= 100:
+			arabic -= 100
+			rn += "C"
+		case arabic >= 90:
+			arabic -= 90
+			rn += "XC"
+		case arabic >= 50:
+			arabic -= 50
+			rn += "L"
+		case arabic >= 40:
+			arabic -= 40
+			rn += "XL"
+		case arabic >= 10:
+			arabic -= 10
+			rn += "X"
+		case arabic >= 9:
+			arabic -= 9
+			rn += "IX"
+		case arabic >= 5:
+			arabic -= 5
+			rn += "V"
+		case arabic >= 4:
+			arabic -= 4
+			rn += "IV"
+		case arabic >= 1:
+			arabic--
+			rn += "I"
+		}
+	}
+	return rn
 }
 
-// type romanNumeralRank struct {
-// 	rank   int
-// 	arabic int
-// 	roman  string
-// }
-
-// //todo: Make this a map with rank as key
-// var romanNumeralRanks = []romanNumeralRank{
-// 	{7, 1000, "M"},
-// 	{6, 500, "D"},
-// 	{5, 100, "C"},
-// 	{4, 50, "L"},
-// 	{3, 10, "X"},
-// 	{2, 5, "V"},
-// 	{1, 1, "I"},
-// }
-
-// func makeRomanNumeral(a int) string {
-
-// 	rn := ""
-// 	for a > 0 {
-// 		for _, v := range romanNumeralRanks {
-// 			if a - v.arabic > 0 {
-// 				rn += v.roman
-// 				a -= v.arabic
-// 				if needsPromoting(rn) {
-// 					rn = promote(rn, v.rank+1)
-// 				}
-// 			}
-// 		}
-// 	}
-// 	return rn
-// }
-
-// func promote(rn string, rank int) string {
-// 	promote := rn[0:len(rn)-3]
-// 	promote += 
-// }
-
-// func needsPromoting(r string) bool {
-// 	if len(r) < 3 {
-// 		return false
-// 	}
-// 	if r[len(r)-3]==r[len(r)-2] && r[len(r)-2] == r[len(r)-1] {
-// 		return true
-// 	}
-// 	return false
-// }
-// //This is the code that converts an int to a group of places:
-// func getPlaces(a int) []int {
-// 	// 	x := 43928
-// 	i := 10
-// 	var places []int
-
-// 	for a > i/10 {
-// 		place := a%i - a%(i/10)
-// 		places = append(places, place)
-// 		i *= 10
-// 	}
-// 	return places
-// }
-
-// }
-// func getPlaces(arabic int) []int {
-// }
+//This is the code that converts an int to a group of places:
+func getPlaces(a int) []int {
+	ds := strconv.Itoa(a)
+	rsp := make([]int, len(ds))
+	pos := 1
+	for i, v := range ds {
+		c := string(v)
+		c += strings.Repeat("0", len(ds)-pos)
+		pos++
+		rsp[i], _ = strconv.Atoi(c)
+	}
+	return rsp
+}
