3bd6ef5026abfeb2d4ff993573bc806552bb9ad4
Scott MacMillan <scott.macmillan@hamiltoncompany.com>
2019-10-21 15:14:35 -0700

    Go Isograms solution

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -1,1 +1,18 @@
 package isogram
+
+import "strings"
+
+//IsIsogram returns true if a word is an isogram
+func IsIsogram(word string) bool {
+	charsUsed := ""
+	for _, v := range word {
+		if string(v) == " " || string(v) == "-" {
+			continue
+		}
+		if strings.Contains(charsUsed, strings.ToLower(string(v))) {
+			return false
+		}
+		charsUsed += strings.ToLower(string(v))
+	}
+	return true
+}
