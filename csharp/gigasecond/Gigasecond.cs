5359d0add3e144fee2706c6e62c27e14284aa200
Scott MacMillan <scott.macmillan@hamiltoncompany.com>
2019-12-12 13:18:05 -0800

    New C# and Python exercise

--- csharp/gigasecond/Gigasecond.cs
+++ csharp/gigasecond/Gigasecond.cs
@@ -4,6 +4,6 @@ public static class Gigasecond
 {
     public static DateTime Add(DateTime moment)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return moment.AddSeconds(1000000000);
     }
 }
