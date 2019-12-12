fca99970df7d8aa1c62e1500448b0103be2d38aa
Scott MacMillan <scott.macmillan@hamiltoncompany.com>
2019-12-12 10:20:18 -0800

    Completed a few CSharp exercises

---
+++ csharp/grains/Grains.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class Grains
+{
+  private static Dictionary<int, ulong> _boardPosition;
+
+  static Grains()
+  {
+    _boardPosition = new Dictionary<int, ulong>();
+    ulong grainCount = 1;
+    _boardPosition[1] = grainCount;
+    for (var i = 2; i <= 64; i++)
+    {
+      grainCount *= 2;
+      _boardPosition[i] = grainCount;
+    }
+  }
+  public static ulong Square(int n)
+  {
+    if (n <= 0 || n > 64)
+    {
+      throw new ArgumentOutOfRangeException("n must be between 1 and 64");
+    }
+    return _boardPosition[n];
+  }
+
+  public static ulong Total()
+  {
+    ulong total = 0;
+    foreach (var value in _boardPosition.Values)
+    {
+      total += value;
+    }
+    return total;
+  }
+
+}
---
+++ csharp/perfect-numbers/PerfectNumbers.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public enum Classification
+{
+    Perfect,
+    Abundant,
+    Deficient
+}
+
+public static class PerfectNumbers
+{
+    public static Classification Classify(int number)
+    {
+      if (number <= 0)
+      {
+        throw new ArgumentOutOfRangeException("number must be greater than zero");
+      }
+        var factors = FindFactors(number);
+        var aliquotSum = factors.Sum();
+       
+        if (aliquotSum == number) 
+        {
+          return Classification.Perfect;
+        }
+        if (aliquotSum > number)
+        {
+          return Classification.Abundant;
+        }
+        return Classification.Deficient;
+    }
+    private static IEnumerable<int> FindFactors(int number)
+    {
+      var factors = new List<int>();
+      for (var x=1; x < number; x++)
+      {
+          if (number % x == 0)
+          {
+            factors.Add(x);
+          }
+      }
+      return factors;
+    }
+    
+}
