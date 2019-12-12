4c584f09229f330418fa9cc2f9b2c3f4db5762ba
Scott MacMillan <scott.macmillan@hamiltoncompany.com>
2019-12-12 15:14:52 -0800

    Fix comments and format in strain.go

--- go/strain/strain.go
+++ go/strain/strain.go
@@ -1,18 +1,19 @@
 package strain
 
-// Ints blah blah blah
+// Ints is an array of ints
 type Ints []int
+
 // Lists blah blah
 type Lists [][]int
 
-// Strings blah blah
+// Strings is an array of strings
 type Strings []string
 
 // Keep returns an array of ints where the predicate returns true
-func (ints Ints ) Keep( predicate func(int) bool) Ints {
+func (ints Ints) Keep(predicate func(int) bool) Ints {
 	var k Ints
 	for _, num := range ints {
-		if (predicate(num)) {
+		if predicate(num) {
 			k = append(k, num)
 		}
 	}
@@ -23,25 +24,25 @@ func (ints Ints ) Keep( predicate func(int) bool) Ints {
 func (ints Ints) Discard(predicate func(int) bool) Ints {
 	var d Ints
 	for _, num := range ints {
-		if (!predicate(num)) {
+		if !predicate(num) {
 			d = append(d, num)
 		}
 	}
 	return d
 }
 
-// Keep blah
-func (lists Lists) Keep( predicate func([]int) bool) Lists {
+// Keep returns Lists where the predicate returns true
+func (lists Lists) Keep(predicate func([]int) bool) Lists {
 	var k Lists
 	for _, arr := range lists {
-		if (predicate(arr)) {
+		if predicate(arr) {
 			k = append(k, arr)
 		}
 	}
 	return k
 }
 
-// Keep blah
+// Keep returns Strings where the predicate returns true
 func (strings Strings) Keep(predicate func(string) bool) Strings {
 	var k Strings
 	for _, s := range strings {
@@ -51,4 +52,3 @@ func (strings Strings) Keep(predicate func(string) bool) Strings {
 	}
 	return k
 }
-
