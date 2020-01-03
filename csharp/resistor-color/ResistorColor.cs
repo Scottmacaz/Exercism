9f4461502a90cb4fe3b691eacba39df8dda5d5bd
Scott MacMillan <scott.macmillan@hamiltoncompany.com>
2020-01-03 14:52:03 -0800

    CSharp ResistorColor exercise

---
+++ csharp/resistor-color/ResistorColor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public static class ResistorColor
+{
+  private static string[] _resistorColors;
+  static ResistorColor() => _resistorColors = new string[]
+      {
+            "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"
+      };
+
+  public static int ColorCode(string color) => Array.IndexOf(_resistorColors, color);
+
+  public static string[] Colors() => _resistorColors;
+}
