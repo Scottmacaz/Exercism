8e2d6115475ae520f6a3d5094c5868f320bc210f
Scott MacMillan <scott.macmillan@hamiltoncompany.com>
2020-01-06 07:24:22 -0800

    Fixed js twofer exercise

--- javascript/two-fer/two-fer.js
+++ javascript/two-fer/two-fer.js
@@ -1,6 +1,3 @@
-export const twoFer = (name) => {
-  if (name) {
-    return `One for ${name}, one for me.`
-  }
-  return 'One for you, one for me.'
+export const twoFer = (name = 'you') => {
+  return `One for ${name}, one for me.`
 };
