b979f18d567b6ee8e33e3f8f69f002554ef3986c
Scott MacMillan <scott.macmillan@hamiltoncompany.com>
2020-01-09 11:05:32 -0800

    hamming python exercise use zip

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,9 +1,9 @@
 def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError("strands must be equal length")
-
+    
     differences = 0
-    for pos, a in enumerate(strand_a):
-        if a != strand_b[pos]:
-            differences += 1
+    for a, b in zip(strand_a, strand_b):
+        if a != b: 
+            differences += 1 
     return differences
