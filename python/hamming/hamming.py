55084cd3063753098009b3dfa21ce8ec401e63cd
Scott MacMillan <scott.macmillan@hamiltoncompany.com>
2020-01-09 11:00:38 -0800

    hamming python exercise make loop enumerate

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -2,11 +2,8 @@ def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError("strands must be equal length")
 
-    position = 0
     differences = 0
-    for a in strand_a:
-        if a != strand_b[position]:
+    for pos, a in enumerate(strand_a):
+        if a != strand_b[pos]:
             differences += 1
-        position += 1
-
     return differences
