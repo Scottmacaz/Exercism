702c33d7ddf22d30d335122773cd58f64deefc9f
= <=>
2020-01-22 19:49:36 -0800

    python allergies solution

--- python/allergies/allergies.py
+++ python/allergies/allergies.py
@@ -1,49 +1,21 @@
 import math
 
 class Allergies:
-    names = ["eggs", "peanuts", "shellfish", "strawberries",
+    NAMES = ["eggs", "peanuts", "shellfish", "strawberries",
              "tomatoes", "chocolate", "pollen", "cats"]
 
     def __init__(self, score):
-        self.score = score
-
+        self.scores = []
+        for i, name in enumerate(self.NAMES):
+            if (1 << i) & score:
+                self.scores.append(name)
+                
     def allergic_to(self, item):
-        
-        # for i in range(len(self.names)):
-        #     print(f"name: {self.names[i]}  value:{2 ** i}")
-
-        # if self.score == 1:
-        #     if item == "eggs":
-        #         return True
-        #     return False
-        
-        name_index = self.names.index(item)
-        print (f"name_index: {name_index}")
-        name_value = 2 ** name_index
-        print(f"name_value: {name_value}")
-        
-         
-       
-        
-        if s <= self.score:
-            return True
-        return False
-
-    def _is_perfect_square(self,num) :
-        root = math.sqrt(num)
-        return int(root + .5) ** 2 == num
-        
-
+       return item in self.scores
+   
     @property
     def lst(self):
-        pass
-
-
-# c = Allergies(2)
-# print(c.allergic_to("peanuts"))
+        return self.scores
 
 c = Allergies(2)
 print(c.allergic_to("eggs"))
-
-# c = Allergies(2)
-# print(c.allergic_to("shellfish"))
